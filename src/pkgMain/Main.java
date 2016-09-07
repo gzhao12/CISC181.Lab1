b0530a932f1c92c06d995b1f1c93dd3f3c06caed
gzhao12 <grantz@udel.edu>
2016-09-07 00:45:18 -0400

    Update 1

--- src/pkgMain/Main.java
+++ src/pkgMain/Main.java
@@ -4,31 +4,37 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		double investValue = -1;
+		double paybackValue = -1;
 		
 		System.out.print("Enter the amount of years you plan to work: ");
 		int workYrs = input.nextInt();
 		
-		System.out.print("Enter your investment interest rate (in %): ");
-		double investInterest = input.nextDouble()/100;
+		while (investValue < 0 || investValue > .2) {
+			System.out.print("Enter your investment interest rate between 0 and 20 (in %): ");
+			investValue = input.nextDouble()/100;
+		}
 		
 		System.out.print("Enter the amount of years you plan to be retired for: ");
 		int retiredYrs = input.nextInt();
 		
-		System.out.print("Enter your payback interest rate (in %): ");
-		double paybackInterest = input.nextDouble()/100;
+		while (paybackValue < 0 || paybackValue > .03) {
+			System.out.print("Enter your payback interest rate between 0 and 3 (in %): ");
+			paybackValue = input.nextDouble()/100;
+		}
 		
-		System.out.print("Enter how much money you would like to withdraw each month during retirement: ");
+		System.out.print("Enter how much money you need each month during retirement: ");
 		double income = input.nextDouble();
 		
 		System.out.print("Enter your monthly SSI: ");
 		double monthlySSI = input.nextDouble();
 		
-		double anualWithdraw = 12*(income-monthlySSI);
-		//double amountWithdrawn = retiredYrs*anualWithdraw;
+		double anualWithdraw = 12*(income-monthlySSI);      
+		double amountWithdrawn = retiredYrs*anualWithdraw;
 		
-		double amountSaved = ((Math.pow(1 + paybackInterest,retiredYrs) - 1)*(anualWithdraw/paybackInterest))/Math.pow(1 + paybackInterest, retiredYrs);
-		double monthlySavings = amountSaved/((Math.pow(1 + investInterest,workYrs)-1)*(1/investInterest));
-		System.out.println("You must save: $" + amountSaved);
-		System.out.println("You must save: $" + monthlySavings + " each month.");
+		double amountSaved = ((Math.pow(1 + paybackValue,retiredYrs) - 1)*(anualWithdraw/paybackValue))/Math.pow(1 + paybackValue, retiredYrs);
+		double monthlySavings = amountSaved/((Math.pow(1 + investValue,workYrs)-1)*(1/investValue));
+		System.out.println("You must save: $" + amountSaved + " before you retire.");
+		System.out.println("You must save: $" + monthlySavings + " each month in order to retire after " + workYrs + " years.");
 	}
 }
